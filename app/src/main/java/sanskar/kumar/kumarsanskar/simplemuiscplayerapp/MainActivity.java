2f1a9f022648e7e8b7f7635aa035027c8db96148
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-18 03:06:11 +0530

    created object for class Button and MediaPlayer.

--- app/src/main/java/sanskar/kumar/kumarsanskar/simplemuiscplayerapp/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/simplemuiscplayerapp/MainActivity.java
@@ -1,12 +1,17 @@
 package sanskar.kumar.kumarsanskar.simplemuiscplayerapp;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button playButton, pauseButton;
+    MediaPlayer mediaPlayer;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
